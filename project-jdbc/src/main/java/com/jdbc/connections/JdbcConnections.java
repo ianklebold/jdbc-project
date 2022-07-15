d6583fbc946ec295558ec61735d8ba4d27d827ec
ianklebold <ianstgo@gmail.com>
2022-07-15 19:39:27 -0300

    Corriendo script

--- project-jdbc/src/main/java/com/jdbc/connections/JdbcConnections.java
+++ project-jdbc/src/main/java/com/jdbc/connections/JdbcConnections.java
@@ -1,8 +1,12 @@
 package com.jdbc.connections;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.h2.tools.RunScript;
 
 public class JdbcConnections {
     public static void main(String[] args) {
@@ -12,9 +16,11 @@ public class JdbcConnections {
             Connection connection = DriverManager.getConnection("jdbc:h2:~/test");
             System.out.println("Conexion creada");
 
+            RunScript.execute(connection, new FileReader("src/main/java/com/jdbc/scripts/scripts_1.sql"));
+            System.out.println("Script executed");
+
             connection.close(); //Desconectarme de la base de datos
-            System.out.println("Conexion cerrada");
-        } catch (SQLException e) {
+        } catch (SQLException | FileNotFoundException e) {
             // Se impirme el error en la pantalla
             e.printStackTrace();
         }
---
+++ project-jdbc/src/main/java/com/jdbc/scripts/scripts_1.sql
@@ -0,0 +1,8 @@
+drop table if exists person;
+
+create table person(
+    id int auto_increment primary key,
+    name varchar(100) not null,
+    lastname varchar(100) not null,
+    nickname varchar(100)
+);
