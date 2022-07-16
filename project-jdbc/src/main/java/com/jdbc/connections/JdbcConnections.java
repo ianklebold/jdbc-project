eeaa31c108302c956198ad0f78028f8af9b3919a
ianklebold <ianstgo@gmail.com>
2022-07-15 21:11:19 -0300

    Implementando ExecuteQuery

--- project-jdbc/src/main/java/com/jdbc/connections/JdbcConnections.java
+++ project-jdbc/src/main/java/com/jdbc/connections/JdbcConnections.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.h2.tools.RunScript;
@@ -20,18 +21,46 @@ public class JdbcConnections {
             RunScript.execute(connection, new FileReader("src/main/java/com/jdbc/scripts/scripts_1.sql"));
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO person(name, lastname, nickname) VALUES(?, ?, ?)");
             
-            preparedStatement.setString(1,"Ian");
-            preparedStatement.setString(2,"Fernández");
-            preparedStatement.setString(3,"iancarnivor");
 
-            preparedStatement.executeUpdate();
-            System.out.println("Script executed");
+            createPersons(preparedStatement);
+            giveMeAllPersons(connection);
 
             preparedStatement.close();
             connection.close(); //Desconectarme de la base de datos
         } catch (SQLException | FileNotFoundException e) {
             // Se impirme el error en la pantalla
             e.printStackTrace();
+        }
+    }
+
+    private static void createPersons(PreparedStatement preparedStatement) throws SQLException {
+
+        preparedStatement.setString(1,"Ian");
+        preparedStatement.setString(2,"Fernández");
+        preparedStatement.setString(3,"iancarnivor");
+        preparedStatement.executeUpdate();
+
+        preparedStatement.setString(1,"Jefferson");
+        preparedStatement.setString(2,"Farfan");
+        preparedStatement.setString(3,"Foquita");
+        preparedStatement.executeUpdate();
+
+        preparedStatement.setString(1,"Brok");
+        preparedStatement.setString(2,"Lessnar");
+        preparedStatement.setString(3,"Broky");
+        preparedStatement.executeUpdate();
+    }
+
+    private static void giveMeAllPersons( Connection connection ) throws SQLException {
+        ResultSet resultSet = connection.prepareStatement("SELECT * FROM person").executeQuery();
+        /**
+         * \n Es un salto de linea
+         * \t Es un tabulador
+         * Se usa printf para poder añadir datos en un string
+         *  */    
+        while (resultSet.next()) {
+            System.out.printf("\n Id: [%d]\tName : [%s]\tLastName : [%s]\tNickName : [%s]",
+            resultSet.getInt(1), resultSet.getString(2),resultSet.getString(3),resultSet.getString(4));
         }
     }
 }
